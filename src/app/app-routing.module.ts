0522d6a9db93e5b2328f1e0e8d74b56c417be01f
Rohit Selvam :) <58054687+Rotitan@users.noreply.github.com>
2022-08-24 20:49:05 +0000

    added routing for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
 
 const routes: Routes = [
+  { path: 'placeOrder', component: PlaceOrderComponent },
 // { path: 'aboutUs', component: AboutUsComponent },
 // { path: 'index', component: IndexComponent},
 // { path: 'products', component: ProductsComponent }
