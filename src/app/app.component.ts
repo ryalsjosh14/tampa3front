ca1b26a77f4d34a1b96e46a9c57c4eb479c885bf
Eduardo Fajardo <emakenso@outlook.es>
2022-08-24 19:45:07 +0000

    Created order history component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+//Need to add app-order-history to the template
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  
 })
 export class AppComponent {
   title = 'tampa3front';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderHistoryComponent
   ],
   imports: [
     BrowserModule,
